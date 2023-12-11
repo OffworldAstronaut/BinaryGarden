08107d201d4328dceca794d3450487f9ad07388d
OffworldAstronaut <rafael.science.amorim@gmail.com>
2023-12-10 23:03:46 -0300

    adição de arquivos para o pacote

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ArvoreBinaria',
+    version='0.1', 
+    description='Um módulo que simula árvores binárias e max-heaps por meio da POO', 
+    author='Rafael Amorim',
+    author_email='rsa16@aluno.ifal.edu.br', 
+    packages=['JogoVelha'],
+)
